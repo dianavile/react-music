eb3d95e0ff2230caa166c8c41b3563ac0b45f1da
dianavile <diana.vile@gmail.com>
2021-02-19 12:37:20 +0100

    update draggable input slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -29,6 +29,10 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2) //format time  in sec, minutes
     );
   };
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({...songInfo, currentTime: e.target.value});
+  };
   //state
   const [songInfo, setSongInfo] = useState({
     currentTime: null,
@@ -38,7 +42,13 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     <div className="player-container">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type="range"
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
